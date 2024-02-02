4ceb8d001e77ae96497030c9eca972814b5ca43f
Russel <russelkas6@gmail.com>
2024-02-02 20:11:26 +0300

    first version

---
+++ src/operations.py
@@ -0,0 +1,34 @@
+import json
+from utils import *
+
+
+def normal_output(file_name):
+    with open(file_name, encoding='utf-8') as f:
+        operations = json.load(f)
+
+    executed_five = []
+    i = 0
+
+    for operation in operations[::-1]:
+        if i < 5:
+            if operation['state'] == 'EXECUTED':
+                executed_five.append(operation)
+                i += 1
+
+    executed_five = sort_data(executed_five)
+
+    for i in range(len(executed_five)):
+        if 'from' in executed_five[i]:
+            print(f'{data_normal(executed_five[i]['date'])} {executed_five[i]['description']}\n'
+                  f'{saving_card_from(executed_five[i]['from'])} -> {saving_card_to(executed_five[i]['to'])}\n'
+                  f'{executed_five[i]['operationAmount']['amount']} {executed_five[i]['operationAmount']['currency']['name']}')
+        else:
+            print(f'{data_normal(executed_five[i]['date'])} {executed_five[i]['description']}\n'
+                  f'-> {saving_card_to(executed_five[i]['to'])}\n'
+                  f'{executed_five[i]['operationAmount']['amount']} {executed_five[i]['operationAmount']['currency']['name']}')
+        print()
+    return ''
+
+
+if __name__ == '__main__':
+    normal_output('operations.json')
---
+++ src/utils.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+
+def sort_data(listing):
+    return sorted(listing, key=lambda x: datetime.strptime(x['date'], "%Y-%m-%dT%H:%M:%S.%f"), reverse=True)
+
+
+def data_normal(string):
+    string_split = string.split('T')
+    string_split_more = string_split[0].split('-')
+    return f'{string_split_more[2]}.{string_split_more[1]}.{string_split_more[0]}'
+
+
+def saving_card_from(string):
+    string_split = string.split(' ')
+    card = [i for i in string_split[1]]
+    count = 0
+    for _ in card:
+        if len(card) == 16:
+            if count in range(6, 12):
+                card[count] = '*'
+            count += 1
+        elif len(card) == 20:
+            if count in range(6, 16):
+                card[count] = '*'
+            count += 1
+    save_card = ''
+    k = 1
+    for i in card:
+        if k % 4 == 0:
+            save_card += i + ' '
+        else:
+            save_card += i
+        k += 1
+    return f'{string_split[0]} {save_card}'
+
+
+def saving_card_to(string):
+    string_split = string.split(' ')
+    return f'{string_split[0]} **{string_split[1][-4:]}'
